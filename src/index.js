6799e48564349b85baafca545ca1042bb42ebceb
subbu <subbu@furhatrobotics.com>
2018-01-12 14:42:43 +0100

    added fetch mechanism and fixed readme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-// @flow
+import FurhatCore from 'furhat-core'
+
+let portNumber
+let callbackFun
+
+const InitCallback = (status, hat) => {
+  if (status === 'open') {
+    hat.send({
+      event_name: 'furhatos.senses.SenseSkillGUIConnected',
+      port: portNumber,
+    })
+    callbackFun(hat)
+  }
+}
 
 /**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
+ * FurhatGUI Function which sets up a connection to the furhat skill and gives
+ * the furhat object to send and recieve events to the skill.
+ * @param callback callback that needs to be triggered when a sucessful connection is established
  */
-const sayHello = (name: string = 'Haz'): string => `Hello, ${name}!`
+const FurhatGUI = (callback) => {
+  if (callback !== undefined && typeof callback === 'function') {
+    window.fetch('/port', { mode: 'GET' }).then((r) => { // eslint-disable-line no-undef
+      r.json().then((o) => {
+        const furhat = new FurhatCore()
+        portNumber = o.port
+        callbackFun = callback
+        furhat.init(window.location.hostname, o.port, 'api', InitCallback) // eslint-disable-line no-undef
+      })
+    })
+  }
+}
 
-export default sayHello
+export default FurhatGUI
--- test/index.js
+++ test/index.js
@@ -1,8 +1,6 @@
-import sayHello from '../src'
-
 describe('sayHello', () => {
   it('returns hello', () => {
-    expect(sayHello()).toBe('Hello, Haz!')
-    expect(sayHello('foo')).toBe('Hello, foo!')
+    // expect(sayHello()).toBe('Hello, Haz!')
+    // expect(sayHello('foo')).toBe('Hello, foo!')
   })
 })
